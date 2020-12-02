3166f862debc0f1d948914cc85b1a740f169e6f2
Triv-johan <guyon.johan@laposte.net>
2020-12-02 15:22:41 +0100

    gfgftgf

---
+++ script.js
@@ -0,0 +1,391 @@
+document.getElementById('bg-task').addEventListener("click", hide);
+
+    function hide() {
+      document.getElementById('bg-task').style.display = "none";
+      $('#bg-task>div').hide();
+    }
+
+    document.getElementById('1').addEventListener('click', a);
+
+    function a() {
+      let GivenDate = '2019-12-01';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Sorry you can't open this window yet");
+      }
+      else {
+        document.getElementById('bg-task').style.display = "block";
+        document.getElementById('a').style.display = "block";
+      }
+    }
+
+
+    document.getElementById('2').addEventListener('click', b);
+
+    function b() {
+      let GivenDate = '2019-12-02';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Sorry you can't open this window yet");
+      }
+      else {
+        document.getElementById('bg-task').style.display = "block";
+        document.getElementById('b').style.display = "block";
+      }
+    }
+
+    document.getElementById('3').addEventListener('click', c);
+
+    function c() {
+      let GivenDate = '2019-12-03';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Sorry you can't open this window yet");
+      }
+      else {
+        document.getElementById('bg-task').style.display = "block";
+        document.getElementById('c').style.display = "block";
+      }
+    }
+
+    document.getElementById('4').addEventListener('click', d);
+
+    function d() {
+      let GivenDate = '2019-12-04';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Sorry you can't open this window yet");
+      }
+      else {
+        document.getElementById('bg-task').style.display = "block";
+        document.getElementById('d').style.display = "block";
+      }
+    }
+
+    document.getElementById('5').addEventListener('click', e);
+
+    function e() {
+      let GivenDate = '2019-12-05';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Sorry you can't open this window yet");
+      }
+      else {
+        document.getElementById('bg-task').style.display = "block";
+        document.getElementById('e').style.display = "block";
+      }
+    }
+
+    document.getElementById('6').addEventListener('click', f);
+
+    function f() {
+      let GivenDate = '2019-12-06';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Sorry you can't open this window yet");
+      }
+      else {
+        document.getElementById('bg-task').style.display = "block";
+        document.getElementById('f').style.display = "block";
+      }
+    }
+
+    document.getElementById('7').addEventListener('click', g);
+
+    function g() {
+      let GivenDate = '2019-12-07';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Sorry you can't open this window yet");
+      }
+      else {
+        document.getElementById('bg-task').style.display = "block";
+        document.getElementById('g').style.display = "block";
+      }
+    }
+
+    document.getElementById('8').addEventListener('click', h);
+
+    function h() {
+      let GivenDate = '2019-12-08';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Sorry you can't open this window yet");
+      }
+      else {
+        document.getElementById('bg-task').style.display = "block";
+        document.getElementById('h').style.display = "block";
+      }
+    }
+
+    document.getElementById('9').addEventListener('click', i);
+
+    function i() {
+      let GivenDate = '2019-12-09';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Sorry you can't open this window yet");
+      }
+      else {
+        document.getElementById('bg-task').style.display = "block";
+        document.getElementById('i').style.display = "block";
+      }
+    }
+
+    document.getElementById('10').addEventListener('click', j);
+
+    function j() {
+      let GivenDate = '2019-12-10';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Sorry you can't open this window yet");
+      }
+      else {
+        document.getElementById('bg-task').style.display = "block";
+        document.getElementById('j').style.display = "block";
+      }
+    }
+
+    document.getElementById('11').addEventListener('click', k);
+
+    function k() {
+      let GivenDate = '2019-12-11';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Sorry you can't open this window yet");
+      }
+      else {
+        document.getElementById('bg-task').style.display = "block";
+        document.getElementById('k').style.display = "block";
+      }
+    }
+
+    document.getElementById('12').addEventListener('click', l);
+
+    function l() {
+      let GivenDate = '2019-12-12';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Sorry you can't open this window yet");
+      }
+      else {
+        document.getElementById('bg-task').style.display = "block";
+        document.getElementById('l').style.display = "block";
+      }
+    }
+
+    document.getElementById('13').addEventListener('click', m);
+
+    function m() {
+      let GivenDate = '2019-12-13';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Sorry you can't open this window yet");
+      }
+      else {
+        document.getElementById('bg-task').style.display = "block";
+        document.getElementById('m').style.display = "block";
+      }
+    }
+
+    document.getElementById('14').addEventListener('click', n);
+
+    function n() {
+      let GivenDate = '2019-12-14';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Sorry you can't open this window yet");
+      }
+      else {
+        document.getElementById('bg-task').style.display = "block";
+        document.getElementById('n').style.display = "block";
+      }
+    }
+
+    document.getElementById('15').addEventListener('click', o);
+
+    function o() {
+      let GivenDate = '2019-12-15';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Sorry you can't open this window yet");
+      }
+      else {
+        document.getElementById('bg-task').style.display = "block";
+        document.getElementById('o').style.display = "block";
+      }
+    }
+
+    document.getElementById('16').addEventListener('click', p);
+
+    function p() {
+      let GivenDate = '2019-12-16';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Sorry you can't open this window yet");
+      }
+      else {
+        document.getElementById('bg-task').style.display = "block";
+        document.getElementById('p').style.display = "block";
+      }
+    }
+
+    document.getElementById('17').addEventListener('click', r);
+
+    function r() {
+      let GivenDate = '2019-12-17';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Sorry you can't open this window yet");
+      }
+      else {
+        document.getElementById('bg-task').style.display = "block";
+        document.getElementById('r').style.display = "block";
+      }
+    }
+
+    document.getElementById('18').addEventListener('click', s);
+
+    function s() {
+      let GivenDate = '2019-12-18';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Sorry you can't open this window yet");
+      }
+      else {
+        document.getElementById('bg-task').style.display = "block";
+        document.getElementById('s').style.display = "block";
+      }
+    }
+
+    document.getElementById('19').addEventListener('click', t);
+
+    function t() {
+      let GivenDate = '2019-12-19';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Sorry you can't open this window yet");
+      }
+      else {
+        document.getElementById('bg-task').style.display = "block";
+        document.getElementById('t').style.display = "block";
+      }
+    }
+
+    document.getElementById('20').addEventListener('click', u);
+
+    function u() {
+      let GivenDate = '2019-12-20';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Sorry you can't open this window yet");
+      }
+      else {
+        document.getElementById('bg-task').style.display = "block";
+        document.getElementById('u').style.display = "block";
+      }
+    }
+
+    document.getElementById('21').addEventListener('click', w);
+
+    function w() {
+      let GivenDate = '2019-12-21';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Sorry you can't open this window yet");
+      }
+      else {
+        document.getElementById('bg-task').style.display = "block";
+        document.getElementById('w').style.display = "block";
+      }
+    }
+
+    document.getElementById('22').addEventListener('click', x);
+
+    function x() {
+      let GivenDate = '2019-12-22';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Sorry you can't open this window yet");
+      }
+      else {
+        document.getElementById('bg-task').style.display = "block";
+        document.getElementById('x').style.display = "block";
+      }
+    }
+
+    document.getElementById('23').addEventListener('click', y);
+
+    function y() {
+      let GivenDate = '2019-12-23';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Sorry you can't open this window yet");
+      }
+      else {
+        document.getElementById('bg-task').style.display = "block";
+        document.getElementById('y').style.display = "block";
+      }
+    }
+
+    document.getElementById('24').addEventListener('click', z);
+
+    function z() {
+      let GivenDate = '2019-12-24';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Sorry you can't open this window yet");
+      }
+      else {
+        document.getElementById('bg-task').style.display = "block";
+        document.getElementById('z').style.display = "block";
+      }
+    }
